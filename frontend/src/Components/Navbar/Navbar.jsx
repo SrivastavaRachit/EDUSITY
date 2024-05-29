6008547f710efcf4ff00658bda3402c5ff7bcddc
SrivastavaRachit <rachitsrivastava@gmail.com>
2024-05-29 17:37:19 +0530

    finished

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png';
+import menu_icon from '../../assets/menu-icon.png';
 import { Link } from 'react-scroll';
 
 
@@ -14,17 +15,23 @@ const Navbar = () => {
         })
     }, []);
 
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const toggleMenu = () => {
+        mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+    }
+
     return (
         <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
             <img src={logo} alt="" className='logo' />
-            <ul>
+            <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
                 <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
                 <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
                 <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link></li>
                 <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
                 <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
-                <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact Us</Link></li>
+                <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
             </ul>
+            <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
         </nav>
     )
 }
