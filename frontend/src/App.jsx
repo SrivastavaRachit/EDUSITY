89e7ee984ae797d64835f493f767ea6c964d720a
SrivastavaRachit <rachitsrivastava@gmail.com>
2024-05-29 16:21:09 +0530

    fourth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,9 @@ import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
@@ -18,6 +21,10 @@ const App = () => {
         <Title subTitle='Gallery' title='Campus Photos' />
         <Campus />
         <Title subTitle='TESTIMONIALS' title='What Student Says' />
+        <Testimonials />
+        <Title subTitle='Contact Us' title='Get In Touch' />
+        <Contact />
+        <Footer />
       </div>
     </div>
   )
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png';
+import { Link } from 'react-scroll';
+
 
 const Navbar = () => {
 
@@ -16,12 +18,12 @@ const Navbar = () => {
         <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
             <img src={logo} alt="" className='logo' />
             <ul>
-                <li>Home</li>
-                <li>Program</li>
-                <li>About Us</li>
-                <li>Campus</li>
-                <li>Testimonials</li>
-                <li><button className='btn'>Contact Us</button></li>
+                <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+                <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
+                <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link></li>
+                <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
+                <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+                <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact Us</Link></li>
             </ul>
         </nav>
     )
